deb692869b8aeded4aca6c8984ce3ca0a31f14fe
Nahuewe <nahuelsoriap@gmail.com>
2025-02-27 09:46:44 -0300

    Protegiendo las rutas con auth:sanctum

--- routes/api.php
+++ routes/api.php
@@ -1,35 +1,31 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\PersonaController;
-use App\Http\Controllers\DocenteController;
-use App\Http\Controllers\SexoController;
-use App\Http\Controllers\AuthController;
-use App\Http\Controllers\FormacionController;
-use App\Http\Controllers\UserController;
-use App\Http\Controllers\RolesController;
+use App\Http\Controllers\{
+    PersonaController, DocenteController, FormacionController,
+    AuthController, SexoController, UserController, RolesController
+};
 
-// Alumnos
-Route::apiResource('personas', PersonaController::class);
-Route::get('personalista', [PersonaController::class,'listapersona']);
-Route::get('personaAll', [PersonaController::class,'personaAll']);
-Route::get('buscar-persona', [PersonaController::class,'buscarPersona']);
-Route::post('/cambiar-estado', [PersonaController::class, 'cambiarEstado']);
-
-// Docentes
-Route::apiResource('docente', DocenteController::class);
-Route::get('docenteAll', [DocenteController::class,'docenteAll']);
-Route::get('buscar-docente', [DocenteController::class, 'buscarDocente']);
-
-// Formacion
-Route::apiResource('formacion', FormacionController::class);
-
-// Auth
+// Rutas públicas
 Route::post('/registrar', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/refresh-token', [AuthController::class, 'refreshToken'])->middleware('auth:sanctum');
 
-// Endpoints extra
-Route::apiResource('sexo', SexoController::class);
-Route::apiResource('/user', UserController::class);
-Route::apiResource('/roles',RolesController::class);
+// Rutas protegidas por middleware auth:sanctum
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('personas', PersonaController::class);
+    Route::get('personalista', [PersonaController::class,'listapersona']);
+    Route::get('personaAll', [PersonaController::class,'personaAll']);
+    Route::get('buscar-persona', [PersonaController::class,'buscarPersona']);
+    Route::post('/cambiar-estado', [PersonaController::class, 'cambiarEstado']);
+
+    Route::apiResource('docente', DocenteController::class);
+    Route::get('docenteAll', [DocenteController::class,'docenteAll']);
+    Route::get('buscar-docente', [DocenteController::class, 'buscarDocente']);
+
+    Route::apiResource('formacion', FormacionController::class);
+
+    Route::apiResource('sexo', SexoController::class);
+    Route::apiResource('/user', UserController::class);
+    Route::apiResource('/roles', RolesController::class);
+});
